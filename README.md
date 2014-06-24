2fa497845f70c450b3c2c9fb8cd678b2886bfa2a
Constantin Titarenko <constantin.titarenko@gmail.com>
2014-06-24 23:22:49 +0300

    Simplified API

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# mymysql
+# sqlcut-mysql
 
-Opinionated wrapper around MySQL client API.
+MySQL API adapter for [sqlcut](https://github.com/titarenko/sqlcut) module.
 
 # API
 
@@ -11,42 +11,11 @@ Opinionated wrapper around MySQL client API.
 Promises array of results.
 
 ```js
+var sqlcut = require('sqlcut');
+var db = sqlcut('sqlcut-mysql', connectionParameters);
+
 db.query('select * from products where name = ?', ['beer']).then(console.log);
 ```
-
-## querySingle(sql, paramsArray)
-
-Promises first row of results.
-
-## insert(tableName, record)
-
-Promises identifier of record that is being inserted.
-
-```js
-db.insert('products', { name: 'beer' }).then(console.log);
-```
-
-## update(tableName, record)
-
-Promises identifier of record that is being updated. Record must have identifier value.
-
-```js
-db.update('products', { id: 20, name: 'fish' }).then(console.log);
-```
-
-## remove(tableName, id)
-
-Promises removal of record with given identifier from specified table.
-
-```js
-db.remove('products', 20).then(function () {
-	console.log('Product 20 was removed');
-});
-```
-
-## find(tableName, id)
-
-Promises record with given identifier taken from specified table.
 
 # License
 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var mysql = require('mysql');
 var Q = require('q');
-var _ = require('lodash');
 
 function SqlError (args, error) {
 	this.name = 'SqlError';
@@ -9,95 +8,34 @@ function SqlError (args, error) {
 	this.message = 'Can\'t complete SQL query due to error.';
 }
 
-var pool;
+function ctor (connectionParameters) {
+	var pool;
 
-function getPool () {
-	return pool || (pool = mysql.createPool(methods.uri));
-}
-
-function query (query, params) {
-	var deferred = Q.defer();
-	getPool().query(query, params, function (error, results) {
-		if (error) {
-			return deferred.reject(new SqlError([query, params], error));
-		}
-		deferred.resolve(results);
-	});
-	return deferred.promise;
-};
-
-function querySingle () {
-	var args = Array.prototype.slice.call(arguments);
-	return query.apply(this, args).then(function (rows) {
-		return rows && rows[0];
-	});
-};
-
-function insert (table, row) {
-	var keys = _.keys(row);
-	var values = _.values(row);
-	var placeholders = keys.map(function () {
-		return '?';
-	});
-
-	var sql = [
-		'insert into', table, '(', keys.join(', '), ')',
-		'values (', placeholders.join(', '), ')'
-	].join(' ');
-
-	return query(sql, values).then(function (result) {
-		return result.insertId;
-	});
-};
-
-function update (table, row) {
-	if (!row.id) {
-		throw new Error('Can\'t update row without id.');
+	function getPool () {
+		return pool || (pool = mysql.createPool(connectionParameters));
 	}
 
-	var pairs = _.pairs(row).filter(function (pair) {
-		return pair[0] != 'id';
-	});
-	var placeholders = pairs.map(function (pair) {
-		return pair[0] + ' = ?';
-	});
-	var values = pairs.map(function (pair) {
-		return pair[1];
-	});
-	values.push(row.id);
+	return function (query, params) {
+		var isInsert = query.indexOf('insert into') == 0;
 
-	var sql = [
-		'update', table, 'set', placeholders.join(', '),
-		'where id = ?'
-	].join(' ');
-	
-	return querySingle(sql, values);
-};
+		var deferred = Q.defer();
+		getPool().query(query, params, function (error, results) {
+			if (error) {
+				return deferred.reject(new SqlError([query, params], error));
+			}
+			deferred.resolve(results);
+		});
 
-function find (table, id) {
-	var sql = ['select * from', table, 'where id = ?'].join(' ');
-	return db.query(sql, [id]);
+		var result = deferred.promise;
+
+		if (isInsert) {
+			result = result.then(function (rows) {
+				return rows.insertId;
+			});
+		}
+
+		return result;
+	};
 }
 
-function remove (table, id) {
-	var sql = ['delete from', table, 'where id = ?'].join(' ');
-	return db.query(sql, [id]);
-}
-
-var methods = {
-	query: query,
-	querySingle: querySingle,
-	insert: insert,
-	update: update,
-	find: find,
-	remove: remove
-};
-
-try {
-	require.resolve('../../config');
-	methods.uri = require('../../config').db.uri;
-} catch (e) {
-	methods.uri = process.env.MSSQL_URI || '';
-}
-
-module.exports = methods;
+module.exports = ctor;
